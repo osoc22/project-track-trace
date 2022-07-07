9a25ea21ff23bc519df0fbce68861352a457ead8
Bo <bo.robbrecht@student.howest.be>
2022-07-07 15:36:24 +0200

    Added License info and configured PWA settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,33 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [
-        /* For more documentation surrounding this, please look at:
-            - https://github.com/kevinmarrec/nuxt-pwa-module
-            - https://pwa.nuxtjs.org/
-        */
-        '@kevinmarrec/nuxt-pwa'
-    ],
-    pwa: {
-        workbox: {
-            enabled: true
-        }
-    },
-    meta: {
-        meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }]
-    }
-})
+	modules: [
+		/*
+		 * For more documentation surrounding this, please look at:
+		 *  - https://github.com/kevinmarrec/nuxt-pwa-module
+		 *  - https://pwa.nuxtjs.org/
+		 */
+		"@kevinmarrec/nuxt-pwa"
+	],
+	pwa: {
+		workbox: {
+			enabled: true
+		},
+		icon: {
+			source:"./assets/icon.png"
+		}
+	},
+	meta: {
+		name: "Paradar",
+		author: "Paradar Team",
+		description: "Paradar allows you to view and track vital goods and personnel in times of chaos",
+		theme_color: "#11A9A1",
+		nativeUI: true
+	},
+	manifest: {
+		name: "Paradar Tracking",
+		short_name: "Paradar",
+		useWebmanifestExtension: "true"
+	}
+});
