7adc5929e6223d9d7e520445f3dd6c57dc9ee46c
Bo <bo.robbrecht@student.howest.be>
2022-07-11 16:45:38 +0200

    Able to load example map

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <Tutorial />
+  <vue-layer-map/>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: "IndexPage"
+});
 </script>
