e4170e57bf3ff7f0d7117d7cde2c7f4dcd70b6a9
Scryper <florian.mazzeo@gmail.com>
2022-07-12 14:06:27 +0200

    Display real time position of tracker

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,83 @@
 <template>
   <vue-layer-map :initial-zoom="6" :initial-center="[4.356998572,50.855996576]">
     <template #features>
-      <vue-layer-marker :coordinates="[4.356998572,50.855996576]" />
+      <vue-layer-marker :coordinates="[baseCoordinates[0], baseCoordinates[1]]" />
     </template>
   </vue-layer-map>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import VueLayerMarker from "~/components/VueLayerMarker.vue";
+    import Vue from "vue";
+    import { connect } from "mqtt";
+    import VueLayerMarker from "~/components/VueLayerMarker.vue";
 
-export default Vue.extend({
-  name: "IndexPage",
-  components: { VueLayerMarker }
-});
+    export default Vue.extend({
+        name: "IndexPage",
+        data () {
+            return {
+                baseCoordinates: [4.356998572, 50.855996576]
+            };
+        },
+        components: { VueLayerMarker },
+        mounted () {
+            example(this.baseCoordinates);
+        }
+    });
+
+    function example (reference: Number[]) {
+        const token = "cnYrjpRhZhECQVkE6CLiowFuAq7pN2rPed7nsOsfVVFAGN2AfBGuXbMlpKB2AQIC";
+        if (token.length !== 64) {
+            console.log("please check the token");
+            return;
+        }
+        const client = connect("wss://mqtt.flespi.io", {
+            clientId: "flespi-examples-mqtt-client-browser",
+            // see https://flespi.com/kb/tokens-access-keys-to-flespi-platform to read about flespi tokens
+            username: "FlespiToken " + token,
+            protocolVersion: 5,
+            clean: true
+        });
+
+        client.on("connect", () => {
+            client.subscribe("flespi/state/gw/devices/4530445/telemetry/#", { qos: 1 }, (err) => {
+                if (err) {
+                    console.log("failed to subscribe to topic \"test\":", err);
+                    return;
+                }
+                console.log("subscribed to \"test\"");
+            });
+        });
+
+        client.on("message", (topic, msg) => {
+            filter(topic, msg.toString("utf8"), reference);
+            /*
+             * console.log("received message in topic \"" + topic + "\": \"" + msg.toString("utf8") + "\"");
+             *
+             * log('disconnecting...');
+             * client.end();
+             */
+        });
+
+        client.on("close", () => {
+            console.log("disconnected");
+        });
+
+        client.on("error", (err) => {
+            console.log("mqtt client error:", err);
+            client.end(true); // force disconnect
+        });
+    }
+
+    function filter (this: any, topic: string, msg: string, ref: Number[]) {
+        const splitTopic: string[] = topic.split("/");
+        const informationName: string = splitTopic[splitTopic.length - 1];
+
+        if (informationName === "position.longitude") {
+            ref[0] = Number(msg);
+        } else if (informationName === "position.latitude") {
+            ref[1] = Number(msg);
+        }
+    }
 </script>
 
 <style lang="scss">
