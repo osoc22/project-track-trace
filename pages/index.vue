b7702cda677f0d6a4d123286b3d129499546e932
Ben Stoffels <beertje.stoffels@proximus.be>
2022-07-12 15:34:06 +0200

    breaking some stuff in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,86 +1,142 @@
 <template>
-  <vue-layer-map :initial-zoom="6" :initial-center="coordinates">
-    <template #features>
-      <vue-layer-marker :coordinates="coordinates" />
-    </template>
-  </vue-layer-map>
+    <vue-layer-map :initial-zoom="6" :initial-center="[longitude, latitude]">
+        <template #features>
+            <vue-layer-marker :coordinates="[longitude, latitude]" />
+        </template>
+    </vue-layer-map>
 </template>
 
 <script lang="ts">
-    import Vue from "vue";
-    import { connect } from "mqtt";
-    import VueLayerMarker from "~/components/VueLayerMarker.vue";
+import Vue from "vue";
+import { connect } from "mqtt";
+import VueLayerMarker from "~/components/VueLayerMarker.vue";
 
-    export default Vue.extend({
-        name: "IndexPage",
-        data () {
-            return {
-                coordinates: [0, 0]
-            };
-        },
-        components: { VueLayerMarker },
-        mounted () {
-            connectToClient(this.coordinates);
-        }
-    });
-
-    // TODO : delete console logs ?
-    function connectToClient (reference: Number[]) {
-        const token = "cnYrjpRhZhECQVkE6CLiowFuAq7pN2rPed7nsOsfVVFAGN2AfBGuXbMlpKB2AQIC"; // TODO : store securely
-        if (token.length !== 64) {
-            console.log("please check the token");
-            return;
-        }
-        const client = connect("wss://mqtt.flespi.io", {
-            clientId: "track-and-trace",
-            // see https://flespi.com/kb/tokens-access-keys-to-flespi-platform to read about flespi tokens
-            username: "FlespiToken " + token,
-            protocolVersion: 5,
-            clean: true
-        });
-
-        // TODO : we should be able to see all trackers and not only one
-        client.on("connect", () => {
-            // Subscribe to the telemetry topic
-            client.subscribe("flespi/state/gw/devices/4530445/telemetry/#", { qos: 1 }, (err) => {
-                if (err) {
-                    console.log("failed to subscribe to topic \"test\":", err);
+export default Vue.extend({
+    name: "IndexPage",
+    data() {
+        return {
+            longitude: 0,
+            latitude: 0
+        };
+    },
+    components: { VueLayerMarker },
+    methods() {
+        return {
+            connectToClient() {
+                const token = "cnYrjpRhZhECQVkE6CLiowFuAq7pN2rPed7nsOsfVVFAGN2AfBGuXbMlpKB2AQIC"; // TODO : store securely
+                if (token.length !== 64) {
+                    console.log("please check the token");
                     return;
                 }
-                console.log("subscribed to \"test\"");
-            });
-        });
+                const client = connect("wss://mqtt.flespi.io", {
+                    clientId: "track-and-trace",
+                    // see https://flespi.com/kb/tokens-access-keys-to-flespi-platform to read about flespi tokens
+                    username: "FlespiToken " + token,
+                    protocolVersion: 5,
+                    clean: true
+                });
 
-        client.on("message", (topic, msg) => {
-            filter(topic, msg.toString("utf8"), reference);
-        });
+                // TODO : we should be able to see all trackers and not only one
+                client.on("connect", () => {
+                    // Subscribe to the telemetry topic
+                    client.subscribe("flespi/state/gw/devices/4530445/telemetry/#", { qos: 1 }, (err) => {
+                        if (err) {
+                            console.log("failed to subscribe to topic \"test\":", err);
+                            return;
+                        }
+                        console.log("subscribed to \"test\"");
+                    });
+                });
 
-        client.on("close", () => {
-            console.log("disconnected");
-        });
+                client.on("message", (topic, msg) => {
+                    this.filter(topic, msg.toString("utf8"));
+                });
 
-        client.on("error", (err) => {
-            console.log("mqtt client error:", err);
-            client.end(true); // force disconnect
-        });
+                client.on("close", () => {
+                    console.log("disconnected");
+                });
+
+                client.on("error", (err) => {
+                    console.log("mqtt client error:", err);
+                    client.end(true); // force disconnect
+                });
+            },
+            filter(topic: string, msg: string) {
+                const splitTopic: string[] = topic.split("/");
+                const informationName: string = splitTopic[splitTopic.length - 1];
+
+                if (informationName === "position.longitude") {
+                    // ref[0] = Number(msg);
+                    this.longitude = Number(msg);
+                } else if (informationName === "position.latitude") {
+                    // ref[1] = Number(msg);
+                    this.latitude = Number(msg);
+                }
+            }
+        };
+    },
+    mounted() {
+        this.connectToClient();
     }
+});
 
-    /*
-     * Filter the data received to keep only the latitude and longitude attributes
-     * TODO : adapt to real needs
-     */
-    function filter (this: any, topic: string, msg: string, ref: Number[]) {
-        const splitTopic: string[] = topic.split("/");
-        const informationName: string = splitTopic[splitTopic.length - 1];
+    // TODO : delete console logs ?
+    // function connectToClient (reference: Number[]) {
+    //     const token = "cnYrjpRhZhECQVkE6CLiowFuAq7pN2rPed7nsOsfVVFAGN2AfBGuXbMlpKB2AQIC"; // TODO : store securely
+    //     if (token.length !== 64) {
+    //         console.log("please check the token");
+    //         return;
+    //     }
+    //     const client = connect("wss://mqtt.flespi.io", {
+    //         clientId: "track-and-trace",
+    //         // see https://flespi.com/kb/tokens-access-keys-to-flespi-platform to read about flespi tokens
+    //         username: "FlespiToken " + token,
+    //         protocolVersion: 5,
+    //         clean: true
+    //     });
 
-        console.log(ref);
-        if (informationName === "position.longitude") {
-            // ref[0] = Number(msg);
-            this.coordinates[0] = Number(msg);
-        } else if (informationName === "position.latitude") {
-            ref[1] = Number(msg);
-        }
-    }
+    //     // TODO : we should be able to see all trackers and not only one
+    //     client.on("connect", () => {
+    //         // Subscribe to the telemetry topic
+    //         client.subscribe("flespi/state/gw/devices/4530445/telemetry/#", { qos: 1 }, (err) => {
+    //             if (err) {
+    //                 console.log("failed to subscribe to topic \"test\":", err);
+    //                 return;
+    //             }
+    //             console.log("subscribed to \"test\"");
+    //         });
+    //     });
+
+    //     client.on("message", (topic, msg) => {
+    //         filter(topic, msg.toString("utf8"), reference);
+    //     });
+
+    //     client.on("close", () => {
+    //         console.log("disconnected");
+    //     });
+
+    //     client.on("error", (err) => {
+    //         console.log("mqtt client error:", err);
+    //         client.end(true); // force disconnect
+    //     });
+    // }
+
+    // /*
+    //  * Filter the data received to keep only the latitude and longitude attributes
+    //  * TODO : adapt to real needs
+    //  */
+    // function filter (this: any, topic: string, msg: string, ref: Number[]) {
+    //     const splitTopic: string[] = topic.split("/");
+    //     const informationName: string = splitTopic[splitTopic.length - 1];
+
+    //     console.log(ref);
+    //     if (informationName === "position.longitude") {
+    //         // ref[0] = Number(msg);
+    //         this.coordinates[0] = Number(msg);
+    //     } else if (informationName === "position.latitude") {
+    //         ref[1] = Number(msg);
+    //     }
+    // }
 </script>
 
 <style lang="scss">
@@ -91,7 +147,10 @@
  *
  * TODO: Export html & body styling to layout component
  */
-html, body, #__nuxt, #__layout{
+html,
+body,
+#__nuxt,
+#__layout {
     height: 100%;
     width: 100%;
 }
