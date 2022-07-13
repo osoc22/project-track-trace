508808130d8eea7c8781cee530cc503adee7e72b
JoshuaMoelans <joshua.moelans@student.uantwerpen.be>
2022-07-13 14:40:42 +0200

    Added comments Updated styling

--- components/FlyOut.vue
+++ components/FlyOut.vue
@@ -24,7 +24,7 @@ export default defineComponent({
       <img id="menu-closed" v-if="!open" src="/svg/burger-menu.svg" width="32" height="32">
       <img id="menu-opened" v-else src="/svg/close-menu.svg" width="32" height="32" >
     </b-button>
-    <b-sidebar id="flyout" bg-variant="dark" no-header shadow>
+    <b-sidebar id="flyout" bg-variant="dark" no-header shadow @hidden="open=false">
       <!-- because of no-header, we need an additional horizontal break-->
       <hr class="flyout-top">
       <SearchTrackers />
@@ -32,7 +32,9 @@ export default defineComponent({
   </div>
 </template>
 
-<style>
+<style lang="scss">
+  $dark-blue: #141f2e;
+  
   #flyout {
     height: 80%;
     width: 15%;
@@ -40,6 +42,7 @@ export default defineComponent({
     border-radius: 0 20px 20px 0;
     position: fixed;
     z-index: -1;
+    background-color: $dark-blue !important;
   }
 
   #flyout-toggle {
@@ -48,15 +51,19 @@ export default defineComponent({
 
     /* needed because of Bootstrap positioning  */
     z-index: 99999;
-    background-color: #343a40;
+    background-color: $dark-blue;
 
     /* important to overwrite basic bootstrap border  */
-    border-radius: 0 7px 7px 0 !important;
+    border-color: $dark-blue !important;
+    border-radius: 0 5px 5px 0 !important;
+  }
+
+  hr {
+    clear: both;
+    visibility: hidden;
   }
 
   .flyout-top {
-    clear: both;
-    visibility: hidden;
     height: 50px;
   }
 </style>
