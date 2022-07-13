10a9d9a7fd3639052dba18140204c8d8ce9ebd05
JoshuaMoelans <joshua.moelans@student.uantwerpen.be>
2022-07-13 10:40:24 +0200

    Improved details pane with details fields Added example Object in SearchTrackers list to hold details of tracker

--- components/DetailsPane.vue
+++ components/DetailsPane.vue
@@ -4,59 +4,64 @@ export default {
         InitOpen: {
             type: Boolean,
             default: false
+        },
+        InitDetails: {
+          type: Object,
+          default() {
+            return {}
+          }
+        },
+        InitName: {
+          type: String,
+          default: ""
         }
     },
     data() {
         return {
-            open: this.InitOpen
+            open: this.InitOpen,
+            details: this.InitDetails,
+            name: this.InitName
         };
     },
     mounted(){
-        this.$root.$on("details", (msg) => {
-            if(! this.open){
+        this.$root.$on("details", (name, trackerDetails) => {
+          // todo - add check if open & name is the same, we can close the details pane again
+          // need to figure out how to overwrite the open=!open from hiding the details
+          if(!this.open){
             document.getElementById("details-toggle").click();
-            }
-            document.getElementById("details-title").innerText = msg;
-            document.getElementById("details-content").innerText = `All information about ${msg} will show up here.`;
+          }
+          this.details = trackerDetails;
+          this.name = name;
         })
-        // Todo - fix bug where, upon closing the details sidebar, the open variable is not reset (easy fix with jquery, but trying to avoid it)
-        //$("#details").on('hidden.bs.collapse', function(){
-        //    this.open = !this.open;
-        //})
     }
 }
 </script>
 
 <template>
   <div id="details-wrapper">
-    <b-button v-b-toggle.details id="details-toggle" style="display: none;" @click="open= ! open">
+    <b-button v-b-toggle.details id="details-toggle" style="display: none;" @click="open=!open">
     </b-button>
-    <b-sidebar id="details" bg-variant="dark" text-variant="light" right shadow>
-        <h2 id="details-title" class="ml-3"></h2>
-        <p id="details-content" class="ml-3"></p>
+    <b-sidebar id="details" bg-variant="dark" text-variant="light" right shadow @hidden="open=!open">
+        <h2 id="details-title" v-if="this.name" class="ml-3">
+          {{ this.name }}
+        </h2>
+        <h4 v-for="(item, index) in details" :key="index" class="ml-4 mr-1">
+          <hr/>
+          {{ index }}
+          <p class="lead">
+            {{ item }}
+          </p>
+        </h4>
     </b-sidebar>
   </div>
 </template>
 
-<style>
+<style lang="scss">
   #details {
     height: 80%;
     width: 15%;
     margin-top: 5%;
     border-radius: 20px 0 0 20px;
     position: fixed;
-    z-index: -1;
-  }
-
-  #details-toggle {
-    margin-top: 0;
-    position: fixed;
-
-    /* needed because of Bootstrap positioning  */
-    z-index: 99999;
-    background-color: #343a40;
-
-    /* important to overwrite basic bootstrap border  */
-    border-radius: 0 7px 7px 0 !important;
   }
 </style>
--- components/SearchTrackers.vue
+++ components/SearchTrackers.vue
@@ -1,26 +1,28 @@
 <script lang="ts">
 import Vue from 'vue'
+
 export default Vue.extend({
     methods: {
-        // temporary function to generate list items -> to be replaced by actual data
-        generateList() {
-            const trackerList = document.getElementById("trackerList");
-            if(trackerList != null){
-                trackerList.innerHTML = "";
-                for (let i = 0; i < 7; i++) {
-                    let li = document.createElement("li");
-                    let name = `Alpha ${String(i)}`;
-                    li.classList.add("list-group-item");
-                    li.innerText = name;
-                    let docRoot = this.$root;
-                    li.addEventListener('click', function(event){
-                        const clickElement = event.target as HTMLElement;
-                        docRoot.$emit("details",clickElement.innerText);
-                    })
-                    trackerList.appendChild(li);
-                }
-            }
+        performSearch(){
+          console.log("performing search");
+        },
+        clickListItem(event : MouseEvent, details : Object) {
+          let targetElement = event.target as HTMLElement;
+          // emit data through root, to be caught by the DetailsPane component
+          this.$root.$emit("details",targetElement.innerText, details);
+        },
+    },
+    data(){
+      return {
+        trackers: {
+          'Alpha 01' : {"description": "Description of Alpha 01","nickname": "Florian"},
+          'Alpha 02' : {"description": "Description of Alpha 02","nickname": "Bo"},
+          'Alpha 03' : {"description": "Description of Alpha 03","nickname": "Ben"},
+          'Alpha 04' : {"description": "Description of Alpha 04","nickname": "Joshua"},
+          'Phone 01' : {"description": "Description of Phone 01","nickname": "Meg"},
+          'Phone 02' : {"description": "Description of Phone 02","nickname": "Jonathan", "groups" : "NCCN, flooding,  kitten stuck in a tree"},
         }
+      }
     }
 })
 </script>
@@ -33,7 +35,7 @@ export default Vue.extend({
             <b-input-group>
               <b-form-input size="sm" placeholder="search tracker"></b-form-input>
               <b-input-group-append>
-                <b-button size="sm" variant="outline-success" @click="generateList">🔍</b-button>
+                <b-button size="sm" variant="outline-success" @click="performSearch">🔍</b-button>
               </b-input-group-append>
             </b-input-group>
           </div>
@@ -44,6 +46,9 @@ export default Vue.extend({
           <div class="col-sm-2"></div>
           <div class="col-sm-8">
             <ul class="list-group" id="trackerList">
+              <li class="list-group-item" v-for="(item, index) in trackers" :key="index" @click="clickListItem($event, item)">
+                {{ index }}
+              </li>
             </ul>
           </div>
           <div class="col-sm-2"></div>
