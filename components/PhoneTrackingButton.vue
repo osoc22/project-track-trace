857e622727784640f17b39ea9b2ba1cdec74d7f0
Ben Stoffels <beertje.stoffels@proximus.be>
2022-07-18 16:56:00 +0200

    Sending geoLocation to Flespi

--- components/PhoneTrackingButton.vue
+++ components/PhoneTrackingButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-button @click="(tracking) ? StopTracking() : StartTracking()">
+  <b-button @click="tracking ? StopTracking() : StartTracking()">
     <b-icon-pin-map />
     <p>{{ tracking ? "Stop Tracking" : "Track me!" }}</p>
   </b-button>
@@ -7,27 +7,58 @@
 
 <script lang="ts">
 import Vue from "vue";
+import { connect, MqttClient } from "mqtt";
 
 export default Vue.extend({
   name: "PhoneTrackingButton",
-  data () {
+  data() {
     return {
       tracking: false,
-      watcherId: -1
+      watcherId: -1,
     };
   },
-  methods: {
-    StartTracking () {
-        this.tracking = true;
-        // Remove the line before when you're implementing this
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        this.watcherId = navigator.geolocation.watchPosition((result) => { /* Write your function here */ });
+  computed: {
+    client() {
+      const token = process.env.FLESPI_KEY;
+
+      const client = connect("wss://mqtt.flespi.io", {
+        clientId: "track-and-trace-2",
+        // see https://flespi.com/kb/tokens-access-keys-to-flespi-platform to read about flespi tokens
+        username: "FlespiToken " + token,
+        protocolVersion: 5,
+        clean: true,
+      });
+
+      client.on("connect", () => console.log("Connected client..."));
+      client.on("disconnect", () => console.log("disconnected client..."));
+
+      return client;
     },
-    StopTracking () {
-        this.tracking = false;
-        navigator.geolocation.clearWatch(this.watcherId);
-        this.watcherId = -1;
-    }
-  }
+  },
+  methods: {
+    StartTracking() {
+      this.tracking = true;
+      console.log("Starting tracker...");
+      this.watcherId = navigator.geolocation.watchPosition((result) => {
+        const data = {
+          ident: "smartphone-tracker",
+          timestamp: result.timestamp / 1000,
+          "position.latitude": result.coords.latitude,
+          "position.longitude": result.coords.longitude,
+        };
+        console.log(data);
+        this.client.publish("paradar/smartphone", JSON.stringify(data), {
+          qos: 0,
+        });
+      });
+    },
+    StopTracking() {
+      this.tracking = false;
+      console.log("Stopping tracker...");
+      navigator.geolocation.clearWatch(this.watcherId);
+      this.client.end(true);
+      this.watcherId = -1;
+    },
+  },
 });
 </script>
