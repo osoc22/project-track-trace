fb92a21af0f4b3915c4fe4a99b5cbb6b5e3db12a
Bo Robbrecht <robbrechtbo@hotmail.com>
2022-07-18 11:17:46 +0200

    Able to fetch location data

--- components/PhoneTrackingButton.vue
+++ components/PhoneTrackingButton.vue
@@ -1,7 +1,7 @@
 <template>
-  <b-button>
+  <b-button @click="(tracking) ? StopTracking() : StartTracking()">
     <b-icon-pin-map />
-    <p>Track device</p>
+    <p>{{ tracking ? "Stop Tracking" : "Track me!" }}</p>
   </b-button>
 </template>
 
@@ -10,5 +10,24 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "PhoneTrackingButton",
+  data () {
+    return {
+      tracking: false,
+      watcherId: -1
+    };
+  },
+  methods: {
+    StartTracking () {
+        this.tracking = true;
+        this.watcherId = navigator.geolocation.watchPosition(result => console.log(result));
+        console.log("Started tracking");
+    },
+    StopTracking () {
+        this.tracking = false;
+        navigator.geolocation.clearWatch(this.watcherId);
+        this.watcherId = -1;
+        console.log("Stop tracking");
+    }
+  }
 });
 </script>
