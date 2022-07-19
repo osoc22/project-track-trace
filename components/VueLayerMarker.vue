99e6b3d0a5aabe14851d9f609c3a79b35b654b29
Bo Robbrecht <robbrechtbo@hotmail.com>
2022-07-19 10:13:28 +0200

    Transfered relevant elements to assets directory from static directory

--- components/VueLayerMarker.vue
+++ components/VueLayerMarker.vue
@@ -23,7 +23,7 @@ export default defineComponent({
         },
         src: {
             type: String,
-            default: "/marker.png"
+            default: "~/assets/images/marker.png"
         }
     }
 });
