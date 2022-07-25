506a929b85c7d39cef35702a2b02f6104e0fa552
Scryper <florian.mazzeo@gmail.com>
2022-07-25 15:51:45 +0200

    Fix merge with master

--- plugins/utils.ts
+++ plugins/utils.ts
@@ -12,7 +12,7 @@ export const eventBus = new Vue(); // creating an event bus.
  * Generates a random ID
  */
 export function generateRandomId (length: number): string {
-    return "_" + Math.random().toString(36).substring(2, 2 + length);
+    return "sp_" + Math.random().toString(36).substring(2, 2 + length);
 }
 
 // ----- Flespi utils - END -----
