5d2cefc38acee10b8dfbf43a0899b61b15cfd5c1
Ben Stoffels <beertje.stoffels@proximus.be>
2022-07-26 10:39:43 +0200

    final problems with merging solved:)

--- components/VueLayerMarker.vue
+++ components/VueLayerMarker.vue
@@ -14,12 +14,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-
-interface Properties {
-  device?: Device,
-  details: Position
-}
+import { defineComponent, PropType } from "vue";
 
 export default defineComponent({
     name: "VueLayerMarker",
@@ -54,48 +49,6 @@ export default defineComponent({
       return {
         iconSrc: this.src
       };
-    },
-    methods: {
-      /**
-       * gets called upon selecting any marker
-       * @param e - the 'event' sent, it looks like {type:string, feature: Feature } where feature is a VueLayers feature
-       */
-      onSelect (e : any) {
-        /*
-         * WARNING: the needed coordinates are regular long/lat (°N °E)
-         * BE SURE TO CONVERT
-         */
-        const markerCoords : Array<number> = e.feature.getGeometry().getCoordinates();
-        const lonlat = toLonLat(markerCoords);
-        // TODO - properly document in our docs
-        /*
-         * WARNING: the object that calls the event is NOT necesarily the correct component for the marker we clicked on
-         * due to this behaviour, we cannot just pass this.details. We use the properties stored in the vl-feature of the component
-         * that was clicked, and extract the details needed. This might also need updating in the future.
-         *
-         * We blame this behaviour on VueLayers being quirky.
-         */
-        const featureProps: Properties = e.feature.getProperties();
-        const positionInfo = featureProps.details;
-        const device = featureProps.device;
-        // convert timestamp to readable format
-        const timestamp : Date = new Date(positionInfo.timestamp * 1000);
-        const tsString : string = timestamp.toLocaleString();
-        const details = Object.fromEntries(Object.entries({
-          ID: positionInfo.id,
-          Longitude: positionInfo.longitude,
-          Latitude: positionInfo.latitude,
-          "Battery Level": positionInfo.batteryLevel,
-          "Last Received Data": tsString
-        }).filter(([_key, value]) => value));
-        if (positionInfo.movementStatus) {
-          details.Moving = undefined;
-        }
-        this.$root.$emit("popup-toggled", lonlat, details, positionInfo.alarmEvent, device?.name);
-      },
-      onDeselect () {
-        this.$root.$emit("popup-hide");
-      }
     }
 });
 </script>
