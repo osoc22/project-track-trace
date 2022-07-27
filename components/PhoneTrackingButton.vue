fdaa0e53636253bd20ae17b1dff0a01f8e5d053c
Meg Vandezande <meg.vandezande@gmail.com>
2022-07-27 10:38:25 +0200

    changes in phone tracking button

--- components/PhoneTrackingButton.vue
+++ components/PhoneTrackingButton.vue
@@ -1,5 +1,8 @@
 <template>
-  <b-button class="d-flex justify-content-center" @click="(tracking) ? stopTracking() : startTracking()">
+  <b-button
+    class="d-flex justify-content-center tracking-button"
+    @click="tracking ? stopTracking() : startTracking()"
+  >
     <b-icon-pin-map />
     <p>{{ tracking ? "Stop Tracking" : "Track me!" }}</p>
   </b-button>
@@ -14,57 +17,68 @@ export default defineComponent({
   props: {
     client: {
       type: MqttClient,
-      required: true
-    }
+      required: true,
+    },
   },
-  data () {
+  data() {
     return {
       tracking: false,
-      watcherId: -1
+      watcherId: -1,
     };
   },
-  beforeDestroy () {
+  beforeDestroy() {
     this.stopTracking();
     this.client.end(true);
   },
   methods: {
-    startTracking () {
-        this.watcherId = navigator.geolocation.watchPosition((result) => {
-          if (!this.tracking) {
-            this.$bvToast.toast("You are being tracked.", {
-              title: "Paradar message",
-              autoHideDelay: 3000,
-              variant: "danger",
-              solid: true,
-              toaster: "b-toaster-top-center"
-            });
-            this.tracking = true;
-          }
-          this.$handleUpdatedPosition(this.client, result);
-        });
-    },
-    stopTracking () {
-        this.tracking = false;
-        navigator.geolocation.clearWatch(this.watcherId);
-        this.watcherId = -1;
-        this.$bvToast.toast("You are not being tracked anymore.", {
+    startTracking() {
+      this.watcherId = navigator.geolocation.watchPosition((result) => {
+        if (!this.tracking) {
+          this.$bvToast.toast("You are being tracked.", {
             title: "Paradar message",
-            autoHideDelay: 3000,
-            variant: "success",
+            autoHideDelay: 200000,
+            variant: "danger",
             solid: true,
-            toaster: "b-toaster-top-center"
-        });
-    }
-  }
+            toaster: "b-toaster-top-center",
+          });
+          this.tracking = true;
+        }
+        this.$handleUpdatedPosition(this.client, result);
+      });
+    },
+    stopTracking() {
+      this.tracking = false;
+      navigator.geolocation.clearWatch(this.watcherId);
+      this.watcherId = -1;
+      this.$bvToast.toast("You are not being tracked anymore.", {
+        title: "Paradar message",
+        autoHideDelay: 3000,
+        variant: "success",
+        solid: true,
+        toaster: "b-toaster-top-center",
+      });
+    },
+  },
 });
 </script>
 
-<style lang="scss" scoped>
-.btn {
-    width: 100%;
-    gap: 0.5rem;
-    p {
-        margin: 0;
-    }
+<style lang="scss">
+@import "../assets/scss/main";
+.tracking-button {
+  border-radius: 0;
+  height: 44px;
+  font-size: 20px;
+  margin-bottom: 20px !important;
+
+  &:hover {
+    color: $paragon-green;
+    background: white;
+  }
+}
+
+.b-toaster-slot * {
+  border-radius: 0;
+  border: none;
+  font-size: 18px;
 }
 </style>
