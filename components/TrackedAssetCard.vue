e628abcc2798bc5f8b4c3cc0c7cf8f137681efe5
Meg Vandezande <meg.vandezande@gmail.com>
2022-07-27 10:38:32 +0200

    changes in tracked asset card

--- components/TrackedAssetCard.vue
+++ components/TrackedAssetCard.vue
@@ -1,36 +1,51 @@
 <template>
-  <b-button class="d-flex justify-content-center" @click="centerMap(position)">
-    {{ device === undefined ? `Smartphone ${position.id}` : `Device ${device.name}` }}
+  <b-button
+    class="d-flex justify-content-center align-items-center"
+    @click="centerMap(position)"
+  >
+    {{
+      device === undefined
+        ? `Smartphone ${position.id}`
+        : `Device ${device.name}`
+    }}
   </b-button>
 </template>
 
 <script lang="ts">
-    import { defineComponent, PropType } from "vue";
-    import { eventBus } from "~/plugins/utils";
+import { defineComponent, PropType } from "vue";
+import { eventBus } from "~/plugins/utils";
 
-    export default defineComponent({
-        name: "TrackedAssetCard",
-        props: {
-            position: {
-                type: Object as PropType<Position>,
-                required: true
-            },
-            // eslint-disable-next-line vue/require-default-prop
-            device: {
-                type: Object as PropType<Device>
-            }
-        },
-        methods: {
-            centerMap (position: Position): void {
-                eventBus.$emit("centerMapOnTrackedAsset", [position.longitude, position.latitude]);
-            }
-        }
-    });
+export default defineComponent({
+  name: "TrackedAssetCard",
+  props: {
+    position: {
+      type: Object as PropType<Position>,
+      required: true,
+    },
+    // eslint-disable-next-line vue/require-default-prop
+    device: {
+      type: Object as PropType<Device>,
+    },
+  },
+  methods: {
+    centerMap(position: Position): void {
+      eventBus.$emit("centerMapOnTrackedAsset", [
+        position.longitude,
+        position.latitude,
+      ]);
+    },
+  },
+});
 </script>
 
 <style lang="scss" scoped>
-.btn {
-    width: 100%;
-    margin-top: 0.5rem;
+button {
+  width: 100%;
+  margin-top: 0.5rem;
+  font-size: 14px;
+  height: 34px;
+  border-radius: 0;
+  background-color: #9da0a2;
+  border-color: #9da0a2;
 }
 </style>
